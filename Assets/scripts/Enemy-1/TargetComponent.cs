5eaab05cea5c502831d0395c08aeac349754223f
Arnab Roy <arnabroy76@gmail.com>
2019-09-01 17:00:08 -0400

    Finetunung combat

--- Assets/scripts/Enemy-1/TargetComponent.cs
+++ Assets/scripts/Enemy-1/TargetComponent.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TargetComponent : MonoBehaviour {
     private Animator anim;
     public Basic2DMovement player;
+    [SerializeField]
     bool isColliding;
 
 
@@ -13,22 +14,35 @@ public class TargetComponent : MonoBehaviour {
         anim = GetComponentInChildren<Animator>();
     }
 
+    public void Update()
+    {
+        if(!player.IsAttacking)
+        {
+            isColliding = false;
+        }
+    }
+
 
     void OnTriggerEnter(Collider other)
 	{
-        //if (isColliding) return;
+        if (isColliding) return;
         isColliding = true;
-        Debug.Log("Got hit");
         if(player.IsAttacking)
         {
-            anim.SetTrigger("headhit");
+            if(player.playerState.Equals(Basic2DMovement.PlayerState.lightattack))
+            {
+                anim.SetTrigger("stomachhit");
+            } else
+            {
+                anim.SetTrigger("headhit");
+            }
+
         }
-        StartCoroutine(AllowCollision());
 	}
 
     IEnumerator AllowCollision()
     {
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(.7f);
         isColliding = false;
     }
 }
